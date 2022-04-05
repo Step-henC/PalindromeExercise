16818e61c145c7cd8b186cdcbae0e7d711be494d
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-05 00:13:58 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            if (str == null || str.Length == 0)
+            { return false; }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[str.Length-1-i])
+                { return true; }
+            }
+            return false;
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,9 +5,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData(null, false)]
+        [InlineData("kayak", true)]
+        [InlineData("mountain", false)]
+        [InlineData("madam", true)]
+        [InlineData("cantankerous", false)]
+        [InlineData("", false)]
+        [InlineData("a    a", true)]
+        
+
+        public void Test1(string str, bool expected)
         {
+            var test = new PalindromeExercise.WordSmith();
+
+            var actual = test.IsAPalindrome(str);
+            Assert.Equal(expected, actual); 
 
         }
     }
